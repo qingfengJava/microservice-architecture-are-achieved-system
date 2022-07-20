8ca67239611229e03e830bf20fac7154a57c94ae
qingfeng <2562777581@qq.com>
2022-07-20 16:34:09 +0800

    word网页版评价表

--- iacaa_analysis/src/main/java/com/qingfeng/analysis/service/impl/WordServiceImpl.java
+++ iacaa_analysis/src/main/java/com/qingfeng/analysis/service/impl/WordServiceImpl.java
@@ -2,9 +2,6 @@ package com.qingfeng.analysis.service.impl;
 
 import cn.afterturn.easypoi.word.WordExportUtil;
 import com.qingfeng.analysis.beans.CourseEntity;
-import com.qingfeng.analysis.beans.vo.CheckLinkVo;
-import com.qingfeng.analysis.beans.vo.CourseTargetVo;
-import com.qingfeng.analysis.beans.vo.CourseTaskVo;
 import com.qingfeng.analysis.dao.CourseDao;
 import com.qingfeng.analysis.dao.TargetDao;
 import com.qingfeng.analysis.service.WordService;
@@ -14,10 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author 清风学Java
@@ -34,15 +28,17 @@ public class WordServiceImpl implements WordService {
 
     @Override
     public void exportWord(Integer courseId, Integer year, HttpServletResponse response) throws Exception {
+        //查询课程信息
+        CourseEntity courseEntity = courseDao.selectById(courseId);
+
         //获取到模板
-        File file = new File("iacaa_analysis/src/main/resources/word_template/word3.docx");
+        File file = new File("iacaa_analysis/src/main/resources/word_template/"+courseEntity.getName()+".docx");
 
         //封装map信息
         HashMap<String, Object> params = new HashMap<>(16);
 
-        //查询课程信息
-        CourseEntity courseEntity = courseDao.selectById(courseId);
-        //封装课程信息
+
+        /*//封装课程信息
         params.put("id",Integer.toString(courseEntity.getId()));
         params.put("name", courseEntity.getName());
 
@@ -83,14 +79,13 @@ public class WordServiceImpl implements WordService {
             targetList.add(targetMap);
         }
 
-        params.put("tgl", targetList);
+        params.put("tgl", targetList);*/
 
         XWPFDocument wordDocument = WordExportUtil.exportWord07(file.getPath(), params);
 
-        System.out.println(params);
 
         // 模板文档结合数据进行操作
-        String filename = "工程教育认证-课程目标达成评价表.docx";
+        String filename = "工程教育认证-"+courseEntity.getName()+"-课程目标达成评价表.docx";
         response.setHeader("content-disposition", "attachment;filename=" + new String(filename.getBytes(), "ISO8859-1"));
         response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         wordDocument.write(response.getOutputStream());
